a212c325098dda50d14bc33318d719fb374c2956
Joseph Arias <arias.joseph.a@gmail.com>
2019-05-25 20:30:10 -0400

    variables and integer datatype

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,14 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, Joseph!");
+
+        // variable myFirstNumber
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 10;
+
+        int myTotal = myFirstNumber + mySecondNumber;
+        System.out.println(myTotal);
     }
 }
