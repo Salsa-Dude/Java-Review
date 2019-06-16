79dc7ef0b387753a88e62e19d27e175ef009c609
Joseph Arias <arias.joseph.a@gmail.com>
2019-06-16 19:28:16 -0400

    String class

---
+++ strings_data_types/src/com/Joseph/Main.java
@@ -0,0 +1,29 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        // *** Sting is not a primitive type, its a class *****
+
+        String myString = "This is a string " + "Joseph";
+        System.out.println(myString);
+
+        String lastString = "10";
+        int myInt = 50;
+
+        lastString = lastString + myInt;
+        System.out.println(lastString); // 1050
+
+
+    }
+}
