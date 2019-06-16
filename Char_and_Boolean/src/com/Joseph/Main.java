44a90ff2eb76f9e933fc911126299b764d99037c
Joseph Arias <arias.joseph.a@gmail.com>
2019-06-15 21:48:41 -0400

    char and boolean

---
+++ Char_and_Boolean/src/com/Joseph/Main.java
@@ -0,0 +1,21 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// char - can only store one letter or one character
+        // " " means string literal
+        //' ' means a individual character
+
+        char myChar = 'A';
+        char uniChar = '\u00A9';
+
+        System.out.println("Hello Letter " + myChar);
+        System.out.println("Your code is " + uniChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        
+    }
+}
