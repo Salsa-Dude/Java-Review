41d4775088f12be5a831679663672ef777e3c74d
Joseph Arias <arias.joseph.a@gmail.com>
2019-09-29 10:21:14 -0400

    keywords and expressions

--- keywords_and_expressions/src/com/Joseph/Main.java
+++ keywords_and_expressions/src/com/Joseph/Main.java
@@ -6,5 +6,13 @@ public class Main {
 	// write your code here
 //        expression is with an variable and value
         double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("Anything inside in a parenthensis in a method is a expression");
+        }
     }
 }
+
+
