0b5f329f1686a71ab66596a7f8f65d3ff45b2c36
Joseph Arias <arias.joseph.a@gmail.com>
2019-09-29 13:29:00 -0400

    java statments

---
+++ src/com/Joseph/Main.java
@@ -0,0 +1,8 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ statements_whitespace/src/com/Joseph/Main.java
@@ -0,0 +1,11 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // Statement includes both datatype and semi colon
+        int myVariable = 50;
+    }
+}
