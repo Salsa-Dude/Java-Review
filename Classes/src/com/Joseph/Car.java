9a0f876140951c524b9fe9b536589e7dd9722543
Joseph Arias <arias.joseph.a@gmail.com>
2019-11-17 19:58:07 -0500

    Classes part 1

---
+++ Classes/src/com/Joseph/Car.java
@@ -0,0 +1,12 @@
+package com.Joseph;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    // public - will have access outside the class
+    // public String model;
+    private String engine;
+    private String color;
+
+}
---
+++ Classes/src/com/Joseph/Main.java
@@ -0,0 +1,13 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+
+
+
+    }
+}
