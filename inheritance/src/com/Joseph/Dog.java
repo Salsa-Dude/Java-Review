c46f2ca5cd81c8b17ae3063a8ba0175fe7179976
Joseph Arias <arias.joseph.a@gmail.com>
2019-11-23 14:43:52 -0500

    inheritance part 1

---
+++ inheritance/src/com/Joseph/Dog.java
@@ -0,0 +1,32 @@
+package com.Joseph;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+
+
+    public Dog( String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        // brain and body variable called using super from base Animal class
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        this.chew();
+        super.eat();
+    }
+}
---
+++ inheritance/src/com/Joseph/Main.java
@@ -0,0 +1,15 @@
+package com.Joseph;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Animal animal = new Animal("Animal", 1, 1, 5,5);
+
+        Dog dog = new Dog("Yorkie", 8, 60, 2, 4, 1, 10, "Fluffy");
+        dog.eat();
+
+    }
+
+
+}
