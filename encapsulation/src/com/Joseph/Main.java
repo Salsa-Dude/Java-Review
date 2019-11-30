225fe991c9d2bc82f0065b5875945ccdb505ef33
Joseph Arias <arias.joseph.a@gmail.com>
2019-11-29 19:03:53 -0500

    encapsulation

---
+++ encapsulation/src/com/Joseph/Main.java
@@ -0,0 +1,21 @@
+package com.Joseph;
+
+// Encapsulation is the mechanism that allows you to restrict access to certain components in objects that you're creating
+// Able to protect the members of a class from external access
+// This allows other classes or code outside of the class from accessing the inner workings of a class.
+// This also enables us to change things without breaking code elsewhere.
+
+// problems without using encapsulation
+// - We give access to change the fields directly
+// - A change in a class variable or method name would lead us to need to update the new name everywhere in our application where its reference.
+// - We don't have a way where the data in a class is valid.
+// No way to validate
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+
+}
