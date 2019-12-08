df9905355767487a0f28cffd0a549ea702c007dd
Joseph Arias <arias.joseph.a@gmail.com>
2019-12-08 01:35:13 -0500

    LinkedList part 1

---
+++ LinkedList/src/com/Joseph/Main.java
@@ -0,0 +1,16 @@
+package com.Joseph;
+
+
+// Linked list is a alternative to arrays
+// How it works is that it actually stores the actual link to the next item in the list
+// - as well as the actual data so hence the name linked list.
+// So in other words each element in the list actually holds a link to the item that
+// - follows it, as well as the actual value you're wanting to actually store as well.
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
