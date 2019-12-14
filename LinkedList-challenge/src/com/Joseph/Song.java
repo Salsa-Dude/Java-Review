7146364e7f76fb7b51b8732a555355fbb426e23f
Joseph Arias <arias.joseph.a@gmail.com>
2019-12-14 16:01:38 -0500

    Setup Song, Album classes

---
+++ LinkedList-challenge/src/com/Joseph/Song.java
@@ -0,0 +1,29 @@
+package com.Joseph;
+
+// Create a Song class having Title and Duration for a song.
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
