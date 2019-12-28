3a74f5fe4f46ba1243f2b8e48514a93264ccf0b2
Joseph Arias <arias.joseph.a@gmail.com>
2019-12-28 17:47:33 -0500

    implemented HeavenlyBody class and Set of planents

---
+++ sets-hashSets/src/com/joseph/Main.java
@@ -0,0 +1,61 @@
+package com.joseph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+	// write your code here
+        HeavenlyBody mercury = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(mercury.getName(), mercury);
+        planets.add(mercury);
+
+        HeavenlyBody venus = new HeavenlyBody("Venus", 225);
+        solarSystem.put(venus.getName(), venus);
+        planets.add(venus);
+
+        HeavenlyBody earth = new HeavenlyBody("Earth", 365);
+        solarSystem.put(earth.getName(), earth);
+        planets.add(earth);
+
+        HeavenlyBody moon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(moon.getName(), moon);
+        earth.addMoon(moon);
+
+        HeavenlyBody mars = new HeavenlyBody("Mars", 687);
+        solarSystem.put(mars.getName(), mars);
+        planets.add(mars);
+
+        HeavenlyBody deimos = new HeavenlyBody("Deimos", 1.3);
+        solarSystem.put(deimos.getName(), deimos);
+        mars.addMoon(deimos); // temp is still Mars
+
+        HeavenlyBody phobos= new HeavenlyBody("Phobos", 0.3);
+        solarSystem.put(phobos.getName(), phobos);
+        mars.addMoon(phobos); // temp is still Mars
+
+        System.out.println("Planets");
+        for(HeavenlyBody planet : planets) {
+            System.out.println("\t" + planet.getName());
+        }
+
+        HeavenlyBody earthBody = solarSystem.get("Earth");
+        System.out.println("Moons of " + earthBody.getName());
+        for(HeavenlyBody earthMoon : earthBody.getSatellites()) {
+            System.out.println("\t" + earthMoon.getName());
+        }
+
+
+
+
+
+
+
+    }
+}
