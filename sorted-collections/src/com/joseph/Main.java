ce7c6b46335c1f866192c10fdf516c880f78628d
Joseph Arias <arias.joseph.a@gmail.com>
2020-01-10 22:57:39 -0500

    StockList class with Maps

--- sorted-collections/src/com/joseph/Main.java
+++ sorted-collections/src/com/joseph/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        StockItem one = new StockItem("test1", 20.00);
+        StockItem two = new StockItem("test2", 30.00);
+        StockItem three = new StockItem("test1", 10.00);
+
+        StockList list = new StockList();
+        list.addStock(one);
+        list.addStock(two);
+        list.addStock(three);
     }
 }
---
+++ sorted-collections/src/com/joseph/StockList.java
@@ -0,0 +1,74 @@
+package com.joseph;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if(item != null) {
+            // check if already have quantities of this item in list
+            StockItem inStock = list.get(item.getName());
+            System.out.println(inStock);
+
+            // if there is already a stock key available in list, adjust the quantity
+            if(inStock != null) {
+                item.adjustStock(inStock.quantityInStock());
+            }
+
+            list.put(item.getName(), item);
+            return item.quantityInStock();
+        }
+
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity) {
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+
+        return 0;
+    }
+
+    public StockItem get(String key) {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List \n";
+        double totalCost = 0.0;
+
+        // The Map.Entry interface enables you to work with a map entry.
+        // The entrySet( ) method declared by the Map interface returns a Set containing the map entries.
+        // Each of these set elements is a Map.Entry object.
+        for(Map.Entry<String, StockItem> item : list.entrySet()) {
+            // getValue() - Returns the value corresponding to this entry.
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            s += stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items:";
+            s += itemValue + "\n";
+
+            totalCost += itemValue;
+
+        }
+
+        return s + " Total stock value " + totalCost;
+    }
+}
