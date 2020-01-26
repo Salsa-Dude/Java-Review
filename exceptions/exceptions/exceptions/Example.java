72cd6787fd37cf39a794e75e7a2be741f85f5638
Joseph Arias <arias.joseph.a@gmail.com>
2020-01-26 17:04:22 -0500

    multi catch exceptions

--- exceptions/exceptions/exceptions/Example.java
+++ exceptions/exceptions/exceptions/Example.java
@@ -9,27 +9,32 @@ import java.util.NoSuchElementException;
 public class Example {
 	
 	public static void main(String[] args) {
-		int result = divide();
-		System.out.println(result);
+		
+		try {
+			int result = divide();
+			System.out.println(result);
+		} catch(ArithmeticException | NoSuchElementException e) {
+			System.out.println(e.toString());
+			System.out.println("unable to perform division, autopilot shutting down");
+		}
+		
 		
 	}
 	
 	private static int divide() {
 		int x, y;
-		try {
+//		try {
 			x = getInt();
 			y = getInt();
-		} catch(NoSuchElementException e) {
-			throw new ArithmeticException("No suitable input");
-		}
-		
-		System.out.println("x is " + x + ", y is " + y);
-		
-		try {
+			System.out.println("x is " + x + ", y is " + y);
 			return x / y;
-		} catch(ArithmeticException e) {
-			throw new ArithmeticException("attempt to divide by 0");
-		}
+			
+//		} catch(NoSuchElementException e) {
+//			throw new ArithmeticException("No suitable input");
+//		} catch(ArithmeticException e) {
+//			throw new ArithmeticException("attempt to divide by 0");
+//		}
+		
 		
 	}
 	
