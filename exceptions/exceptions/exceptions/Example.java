6b92dadab48b2e02d82d80e920413c16d596f271
Joseph Arias <arias.joseph.a@gmail.com>
2020-01-26 15:02:11 -0500

    catching and throwing exceptions

--- exceptions/exceptions/exceptions/Example.java
+++ exceptions/exceptions/exceptions/Example.java
@@ -1,6 +1,10 @@
 package exceptions;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+
+
 
 public class Example {
 	
@@ -11,17 +15,38 @@ public class Example {
 	}
 	
 	private static int divide() {
-		int x = getInt();
-		int y = getInt();
+		int x, y;
+		try {
+			x = getInt();
+			y = getInt();
+		} catch(NoSuchElementException e) {
+			throw new ArithmeticException("No suitable input");
+		}
 		
 		System.out.println("x is " + x + ", y is " + y);
-		return x/y;
+		
+		try {
+			return x / y;
+		} catch(ArithmeticException e) {
+			throw new ArithmeticException("attempt to divide by 0");
+		}
+		
 	}
 	
 	private static int getInt() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter an integer");
 		
-		return s.nextInt();
+		while(true) {
+			try {
+				return s.nextInt();
+			} catch(InputMismatchException e) {
+				// go again. Read past the end of line in the input first
+				s.nextLine();
+				System.out.println("Please enter a number using the digits 0 to 9");
+			}
+		}
+		
+		
 	}
 }
